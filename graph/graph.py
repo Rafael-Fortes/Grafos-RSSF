b61fca3ce4e0587438728f084d2d6b708b27c28c
Rafael Fortes <rafaeldsfortes@gmail.com>
2024-11-13 20:25:15 -0300

    feat: creates de adjacency matrix

--- graph/graph.py
+++ graph/graph.py
@@ -1,45 +1,82 @@
-from typing import Tuple
+from typing import List, Tuple, Dict
 import math
 
 class Sensor:
-    def __init__(self, identifier: str, position: Tuple[int, int], range_radius: float):
+    def __init__(self, identifier: int, position: Tuple[float, float], range_radius: float, is_base_station: bool = False):
         self.identifier = identifier
         self.position = position
         self.range_radius = range_radius
+        self.is_base_station = is_base_station
     
     def distance_to(self, other_sensor: 'Sensor') -> float:
         x1, y1 = self.position
         x2, y2 = other_sensor.position
-        return math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2))
+        euclidian_distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+        return euclidian_distance
     
     def can_communicate_with(self, other_sensor: 'Sensor') -> bool:
         return self.distance_to(other_sensor) <= self.range_radius
 
-
 class SensorNetwork:
     def __init__(self):
-        self.sensors = []
+        self.sensors: Dict[int, Sensor] = {}
+        self.adjacency_matrix: List[List[float]] = []
+        self.qtd_sensors = 0
 
     def add_sensor(self, sensor: Sensor):
-        self.sensors.append(sensor)
-    
+        id = sensor.identifier
+        self.sensors[id] = sensor
+
     def load_from_file(self, file_path: str):
         with open(file_path, 'r') as file:
             lines = file.readlines()
         
-        num_sensors = int(lines[0].strip())
-        base_station_coords = tuple(map(float, lines[1].strip().split(',')))
-        self.base_station = Sensor(identifier="Base", position=base_station_coords, range_radius=0)
+        for i, line in enumerate(lines):
+            if i == 0:
+                num_sensors = int(line.strip())
+                self.qtd_sensors = num_sensors
+            elif i == 1:
+                base_station_coords = tuple(map(float, line.strip().split(',')))
+                sensor = Sensor(identifier=0, position=base_station_coords, range_radius=100.0, is_base_station=True)
+                self.add_sensor(sensor)
+            else:
+                x, y = map(float, line.strip().split(','))
+                sensor = Sensor(identifier=i-1, position=(x, y), range_radius=100.0)
+                self.add_sensor(sensor)
+        
+        self.qtd_sensors = len(self.sensors)
+        print(self.qtd_sensors)
 
-        for i in range(2, 2 + num_sensors):
-            x, y = map(float, lines[i].strip().split(','))
-            sensor = Sensor(identifier=str(i-2), position=(x, y), range_radius=100.0)
-            self.add_sensor(sensor)
+        self.build_adjacency_matrix()
 
-    def get_communication_pairs(self):
-        pairs = []
-        for i, sensor in enumerate(self.sensors):
-            for other_sensor in self.sensors[i+1:]:
-                if sensor.can_communicate_with(other_sensor):
-                    pairs.append((sensor, other_sensor))
-        return pairs
+    def build_adjacency_matrix(self):
+        size = len(self.sensors)
+        self.adjacency_matrix = [[float('inf')] * size for _ in range(size)]
+
+        for sensor_1_id in self.sensors:
+            sensor1 = self.sensors[sensor_1_id]
+
+            for sensor_2_id in self.sensors:
+                sensor2 = self.sensors[sensor_2_id]
+
+                if sensor_1_id != sensor_2_id:
+                    distance = sensor1.distance_to(sensor2)
+                    if sensor1.can_communicate_with(sensor2):
+                        self.adjacency_matrix[sensor_1_id][sensor_2_id] = distance
+                    else:
+                        self.adjacency_matrix[sensor_1_id][sensor_2_id] = float('inf')
+                else:
+                    self.adjacency_matrix[sensor_1_id][sensor_2_id] = 0.0
+    
+    def print_adjacency_matrix(self) -> None:
+        for i in range(self.qtd_sensors):
+            for j in range(self.qtd_sensors):
+                print(f"{self.adjacency_matrix[i][j]: .2f}", end="\t")
+            print()
+
+
+if __name__ == "__main__":
+    file_path = "data/Cenário 4 - Rede 50.txt"
+    graph = SensorNetwork()
+    graph.load_from_file(file_path)
+    graph.print_adjacency_matrix()
--- main.py
+++ main.py
@@ -38,44 +38,52 @@ class Screen:
         pygame.display.flip()
     
     def draw_sensors(self):
-        for sensor in self.sensor_network.sensors:
+        for sensor_id in self.sensor_network.sensors:
+            sensor = self.sensor_network.sensors[sensor_id]
             x, y = sensor.position
             screen_x = int(round((x / 1000) * self.screen.get_width()))
             screen_y = int(round((y / 1000) * self.screen.get_height()))
 
             # Desenhar o raio de alcance
-            range_radius_on_screen = int(round((sensor.range_radius / 1000) * self.screen.get_width()))
-            pygame.draw.circle(self.screen, (0, 0, 255), (screen_x, screen_y), range_radius_on_screen, 1)
+            # range_radius_on_screen = int(round((sensor.range_radius / 1000) * self.screen.get_width()))
+            # pygame.draw.circle(self.screen, (0, 0, 255), (screen_x, screen_y), range_radius_on_screen, 1)
 
             # Desenhar o sensor
             pygame.draw.circle(self.screen, (0, 0, 255), (screen_x, screen_y), 5)
 
-        if self.sensor_network.base_station:
-            x, y = self.sensor_network.base_station.position
-            screen_x = int(round((x / 1000) * self.screen.get_width()))
-            screen_y = int(round((y / 1000) * self.screen.get_height()))
-            pygame.draw.circle(self.screen, (255, 0, 0), (screen_x, screen_y), 8)
+            if sensor.is_base_station:
+                base_station = sensor
+
+        x, y = base_station.position
+        screen_x = int(round((x / 1000) * self.screen.get_width()))
+        screen_y = int(round((y / 1000) * self.screen.get_height()))
+        pygame.draw.circle(self.screen, (255, 0, 0), (screen_x, screen_y), 8)
     
     def draw_communication_lines(self):
-        pairs = self.sensor_network.get_communication_pairs()
-        for sensor1, sensor2 in pairs:
-            x1, y1 = sensor1.position
-            x2, y2 = sensor2.position
-            screen_x1 = int(round((x1 / 1000) * self.screen.get_width()))
-            screen_y1 = int(round((y1 / 1000) * self.screen.get_height()))
-            screen_x2 = int(round((x2 / 1000) * self.screen.get_width()))
-            screen_y2 = int(round((y2 / 1000) * self.screen.get_height()))
-            pygame.draw.line(self.screen, (0, 0, 0), (screen_x1, screen_y1), (screen_x2, screen_y2), 1)
+        for sensor_1_id in range(self.sensor_network.qtd_sensors):
+            sensor1 = self.sensor_network.sensors[sensor_1_id]
+
+            for sensor_2_id in range(self.sensor_network.qtd_sensors):
+                sensor2 = self.sensor_network.sensors[sensor_2_id]
+
+                if sensor1.can_communicate_with(sensor2):
+                    x1, y1 = sensor1.position
+                    x2, y2 = sensor2.position
+                    screen_x1 = int(round((x1 / 1000) * self.screen.get_width()))
+                    screen_y1 = int(round((y1 / 1000) * self.screen.get_height()))
+                    screen_x2 = int(round((x2 / 1000) * self.screen.get_width()))
+                    screen_y2 = int(round((y2 / 1000) * self.screen.get_height()))
+                    pygame.draw.line(self.screen, (0, 0, 0), (screen_x1, screen_y1), (screen_x2, screen_y2), 1)
 
     def __del__(self):
         pygame.quit()
 
 if __name__ == "__main__":
-    qtd_sensors = 50
+    qtd_sensors = 200
     dataset_path = f"data/Cenário 4 - Rede {qtd_sensors}.txt"
 
     sensor_network = SensorNetwork()
     sensor_network.load_from_file(dataset_path)
 
-    visualizer = Screen(width=800, height=800, bg_color=(255, 255, 255), sensor_network=sensor_network)
+    visualizer = Screen(width=600, height=600, bg_color=(255, 255, 255), sensor_network=sensor_network)
     visualizer.run()
